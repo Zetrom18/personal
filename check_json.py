83a327595e079600f266574315dec15d2f125f59
Lucas <lucas.rosa.simepar@gmail.com>
2018-10-10 15:17:46 -0300

    added classes to bash
    Signed-off-by: Lucas <lucas.rosa.simepar@gmail.com>

--- check_json.py
+++ check_json.py
@@ -5,4 +5,8 @@ if len(sys.argv) < 2:
 	exit(1)
 
 with open(sys.argv[1], "r") as f:
-	print(json.loads(f.read()))
+	data = json.loads(f.read())
+	for type in data:
+		for key, value in data[type].items():
+			print(key, value)
+
--- generate_json.sh
+++ generate_json.sh
@@ -10,28 +10,52 @@ for info in ${infoList[@]}
 do
 	echo "  \"$info\": {" >> $outFile
 	fileList=($(ls $infoDir/$info))
-	for file in ${fileList[@]}
-	do
-		echo "    \"$file\": {" >> $outFile
-		while IFS= read -r line || [[ -n "$line" ]]
+	if [[ $info == "races" ]]
+	then
+		for file in ${fileList[@]}
 		do
-			if [[ $info == "races" ]]
-			then
+			echo "    \"$file\": {" >> $outFile
+			while IFS= read -r line || [[ -n "$line" ]]
+			do
 				if [[ $(echo $line | sed 's/[^.]//g' | awk '{ print length }') > 1 ]]
 				then
 					tag=$(echo $line | cut -d'.' -f1)
-					content=$(echo $line | cut -d'.' -f1 --complement)
+					content=$(echo $line | cut -d'.' -f1 --complement | sed '$ s/^ //')
 					echo "      \"$tag\": \"$content\"," >> $outFile
 				fi
+			done < $infoDir/$info/$file
+			sed -i '$ s/.$//' $outFile
+			if [[ $file == ${fileList[-1]} ]]
+			then
+				echo "    }" >> $outFile
+			else
+				echo "    }," >> $outFile
 			fi
-		done < $infoDir/$info/$file
-		if [[ $file == ${fileList[-1]} ]]
-		then
-			echo "    }" >> $outFile
-		else
-			echo "    }," >> $outFile
-		fi
-	done
+		done
+	fi
+	if [[ $info == "classes" ]]
+	then
+		for file in ${fileList[@]}
+		do
+			echo "    \"$file\": {" >> $outFile
+			while IFS= read -r line || [[ -n "$line" ]]
+			do
+				if [[ $(echo $line | sed 's/[^.]//g' | awk '{ print length }') > 1 ]]
+				then
+					tag=$(echo $line | cut -d'.' -f1)
+					content=$(echo $line | cut -d'.' -f1 --complement | sed '$ s/^ //')
+					echo "      \"$tag\": \"$content\"," >> $outFile
+				fi
+			done < $infoDir/$info/$file
+			sed -i '$ s/.$//' $outFile
+			if [[ $file == ${fileList[-1]} ]]
+			then
+				echo "    }" >> $outFile
+			else
+				echo "    }," >> $outFile
+			fi
+		done
+	fi
 	if [[ $info == ${infoList[-1]} ]]
 	then
 		echo "  }" >> $outFile
